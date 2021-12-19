5cfa1cd445c7fc2ccd67f542e6ccf7d929519c74
bnealdefero <bneal@deferousa.com>
2021-12-19 08:58:24 -0700

    changing adapter to union with url adapter

--- models/url_google_ads/google_ads__ad_group_adapter.sql
+++ models/url_google_ads/google_ads__ad_group_adapter.sql
@@ -1,60 +1,38 @@
 {{ config(enabled=var('api_source') == 'google_ads') }}
 
-with stats as (
-
-    select *
-    from {{ var('ad_group_stats') }}
-
-), accounts as (
-
-    select *
-    from {{ var('account') }}
-    
-), campaigns as (
-
-    select *
-    from {{ var('campaign_history') }}
-    where is_most_recent_record = True
-    
-), ad_groups as (
-
-    select *
-    from {{ var('ad_group_history') }}
-    where is_most_recent_record = True
-    
-), fields as (
+-- union our dsa's back into the google_ads_url_ad_adapter
+with unioned as (
+    {{ dbt_utils.union_relations(
+        relations=[ref('stg_google_ads__ad_group_dsas_tmp'), ref('google_ads__url_ad_adapter')]
+    ) }}
+),
+fields as (
 
     select
-        stats.date_day,
-        accounts.account_name,
-        accounts.account_id,
-        campaigns.campaign_name,
-        campaigns.campaign_id,
-        ad_groups.ad_group_name,
-        ad_groups.ad_group_id,
-        lower(stats.ad_network_type) as ad_network_type,
-        sum(stats.spend) as spend,
-        sum(stats.clicks) as clicks,
-        sum(stats.impressions) as impressions
-
+        date_day,
+        lower(account_name) as account_name,
+        account_id,
+        lower(campaign_name) as campaign_name,
+        campaign_id,
+        ad_group_name,
+        ad_group_id,
+        ad_network_type,
+        base_url,
+        url_host,
+        url_path,
+        utm_source,
+        utm_medium,
+        lower(utm_campaign),
+        utm_content,
+        utm_term,
+        spend,
+        clicks,
+        impressions
         {% for metric in var('google_ads__ad_group_stats_passthrough_metrics') %}
-        , sum(stats.{{ metric }}) as {{ metric }}
+        , {{ metric }}
         {% endfor %}
-
-    from stats
-
-    left join ad_groups
-        on stats.id = ad_groups.ad_group_id
-
-    left join campaigns
-        on ad_groups.campaign_id = campaigns.campaign_id
-
-    left join accounts
-        on campaigns.account_id = accounts.account_id
-
-    {{ dbt_utils.group_by(8) }}
-
+    from unioned
 )
 
-select *
-from fields
+select * from fields
+
