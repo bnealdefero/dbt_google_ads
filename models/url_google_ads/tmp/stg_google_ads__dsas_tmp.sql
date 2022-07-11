ab4dfa48a40deb7b2f26329a60ac97e46eff12c1
bnealdefero <bneal@deferousa.com>
2022-07-11 00:28:30 -0700

    removing imc filter exclusion on dsas

--- models/url_google_ads/tmp/stg_google_ads__dsas_tmp.sql
+++ models/url_google_ads/tmp/stg_google_ads__dsas_tmp.sql
@@ -20,7 +20,7 @@ joined as (
     WHERE
         b.campaign_id IS NULL
         -- skiping imc
-        and a.account_id not in(1591985123)
+        -- and a.account_id not in(1591985123)
 )
 
 select * from joined
